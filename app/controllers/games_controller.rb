dff8ace7aef7ebea9a3d7db293731621b9e88db6
Chris <vc.chris.higgins@lowes.com>
2016-12-01 14:03:35 -0500

    First

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,18 @@
+class GamesController < ApplicationController
+	def index
+		@games = Game.all #magically goes to the view
+	end
+	def new
+
+	end
+
+	def create
+		@game = Game.new(params.require(:game).permit(:name))
+		@game.save
+		redirect_to @game #redirects to game_path/id which hits routes
+	end
+
+	def show
+		@game = Game.find(params[:id]) #these @vars are magically sent to the view
+	end
+end
