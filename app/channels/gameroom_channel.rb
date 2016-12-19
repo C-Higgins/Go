7087d7820498340630785966f6c97b3db81e44b3
Chris <vc.chris.higgins@lowes.com>
2016-12-19 17:05:22 -0500

    Shit is fucked

---
+++ app/channels/gameroom_channel.rb
@@ -0,0 +1,21 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+class GameroomChannel < ApplicationCable::Channel
+	def subscribed
+		stream_from "game_#{params['room']}"
+	end
+
+	def unsubscribed
+		# Any cleanup needed when channel is unsubscribed
+	end
+
+	def receive data
+		@game = Game.find_by(webid: data["webid"])
+		history = data["data"]
+		# validate history
+		@game.update_attributes(history: history)
+		if @game.save
+			ActionCable.server.broadcast("game_#{params['room']}", history)
+			puts ''
+		end
+	end
+end
