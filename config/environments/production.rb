25344f64bd836fc2c34c47cbd3805d8137d053d3
Chris Higgins <chrishiggins2012@gmail.com>
2017-02-19 00:32:44 -0500

    redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://rgo.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://rgo.herokuapp.com', 'http://rgo.herokuapp.com']
 end
