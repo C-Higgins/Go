217cf4f585f3117f32322329f3cecc80bb828cfe
Chris Higgins <chrishiggins2012@gmail.com>
2017-02-22 16:39:12 -0500

    temp fix for wrong babel config

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -13,7 +13,7 @@
 }).call(this);
 
 function unsubscribe(channelname) {
-	App.cable.subscriptions.subscriptions.find((s) => {
-		return JSON.parse(s.identifier).channel == channelname
+	App.cable.subscriptions.subscriptions.find(function (s) {
+		return JSON.parse(s.identifier).channel == channelname;
 	}).unsubscribe();
 }
