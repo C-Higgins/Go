1f2d8b162af2b36b3a86181f7db19cf202f28229
Chris Higgins <chrishiggins2012@gmail.com>
2017-02-22 16:42:30 -0500

    same

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -3,7 +3,7 @@
 function createGame(user) {
 	setTimeout(function () { // Necessary to allow the form to exist until it posts
 		$('#modal').remove();
-		if (!App.cable.subscriptions.subscriptions.find((s) => {
+		if (!App.cable.subscriptions.subscriptions.find(function(s) {
 				return JSON.parse(s.identifier).channel == 'WaitingChannel'
 			})) {
 			waitSubscribe(user);
