9666c8fb3b342ee8f776996cd9de4c984ad57e95
Chris <vc.chris.higgins@lowes.com>
2017-02-23 10:47:51 -0500

    Babel stuff

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'sprockets/es6'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,5 +12,18 @@ module Go
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    Rails.application.configure do
+      config.npm.enable_watch                = Rails.env.development?
+
+      # Command to install dependencies
+      config.npm.install                     = ['npm install']
+
+
+      # If 'true', runs 'npm install' on 'rake assets:precompile'. (v1.6.0+)
+      # This is generally desired, but you may set this to false when
+      # deploying to Heroku to speed things up.
+      config.npm.install_on_asset_precompile = false
+    end
   end
 end
