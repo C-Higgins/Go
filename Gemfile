5573ffb2bd669153a607e3e283590ee801ecb149
Chris <vc.chris.higgins@lowes.com>
2017-02-23 10:54:28 -0500

    test2

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 gem 'sprockets-es6'
-gem 'npm-pipeline-rails'
+#gem 'npm-pipeline-rails'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- config/application.rb
+++ config/application.rb
@@ -13,17 +13,17 @@ module Go
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    Rails.application.configure do
-      config.npm.enable_watch                = Rails.env.development?
-
-      # Command to install dependencies
-      config.npm.install                     = ['npm install']
-
-
-      # If 'true', runs 'npm install' on 'rake assets:precompile'. (v1.6.0+)
-      # This is generally desired, but you may set this to false when
-      # deploying to Heroku to speed things up.
-      config.npm.install_on_asset_precompile = true
-    end
+    # Rails.application.configure do
+    #   config.npm.enable_watch                = Rails.env.development?
+	#
+    #   # Command to install dependencies
+    #   config.npm.install                     = ['npm install']
+	#
+	#
+    #   # If 'true', runs 'npm install' on 'rake assets:precompile'. (v1.6.0+)
+    #   # This is generally desired, but you may set this to false when
+    #   # deploying to Heroku to speed things up.
+    #   config.npm.install_on_asset_precompile = true
+    # end
   end
 end
